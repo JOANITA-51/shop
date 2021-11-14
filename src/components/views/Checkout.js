766b9fe706de11cd6be136e37b2dd5814d388268
JOANITA-51 <nakityojoanita51@gmail.com>
2021-11-14 11:43:35 +0300

    generating a group of vouchers

--- src/components/views/Checkout.js
+++ src/components/views/Checkout.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useCart } from '../contexts/Cart'
 
+const [voucher, setVoucher]= useState('')
+
 const Checkout = () => {
     const formRef = useRef('Checkout')
     const {total}= useCart()
     const handlePayment = () =>{
         console.log (1)
     }
+    const vouchers = {
+        xxx:{rate:10, status:'active', amount:10000},
+        yyy:{rate:15, status:'active', amount:10000},
+        www:{rate:20, status:'active', amount:null},
+        zzz:{rate:30, status:'active', amount:10000}
+    }
+
     return (
         
         <div>
@@ -24,13 +33,14 @@ const Checkout = () => {
                 </fieldset>
 
                 <fieldset>
-                    <label>Payment</label>
+                    <label>Cart details</label>
                     <p>Subtotal {total}</p>
                     <p>Shipping {shipping}</p>
                     <p>Discount {discount}</p>
                     <p>Tax {tax}</p>
                     <p>Total {total + shipping + tax - discount}</p>
                 </fieldset>
+
                 <h1>Shipping info</h1>
                 <h1>Payment</h1>
                 <div>
