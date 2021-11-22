b37fbfccbfe76fe13903129b3b151d1bab501654
JOANITA-51 <nakityojoanita51@gmail.com>
2021-11-22 16:13:12 +0300

    express backend

---
+++ backend/routes/patasente.js
@@ -0,0 +1,22 @@
+var axios = require('axios');
+var express = require('express');
+var router = express.Router();
+
+const URL = 'https://patasente.me/phantom-api/send-transaction-token/0634vhfoP/e8ad92b8b2629b0db4651612a24099b7'
+router.post('/', async function(req, res, next){
+    try{
+        const results = await axios.post(URL, req.body)
+        console.log (results.data)
+        res.json({'msg' : 'Api reached!'})
+
+    } catch(error){
+        console.log(error)
+        res.json({
+            'msg':'Api not reached',
+            'your_body':req.body
+        })
+    }
+
+});
+
+module.exports = router;
